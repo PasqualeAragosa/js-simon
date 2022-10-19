2c1a83de0b012cc6bc91b9dde0d868655eabe9df
Pasquale Aragosa <aragosap@gmail.com>
2022-10-19 22:17:33 +0200

    prompt doesn't work

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,61 +6,77 @@
 5 - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
-/*
- - Funzione: genera numeri interi casuali da 1 a 100
- - Inserisco i numeri in un div generico per 5 volte
- - Funzione: Timer 30 secondi
-    - Creo un pulsante tramite il quale i numeri vengono visualizzati
- - L'utente inserisce i numeri che ricorda tramite prompt
- - Faccio un controllo sui numeri che l'utente ha inserito e quelli che ha generato il pc.
- - Variabile per ricordare quanti numeri sono stati indovinati
- - Stampa a video i numeri indovinati
+/* 
+- Appaiono 5 numeri casuali
+    - Funzione: genero 5 numeri
+    - li salvo in un array
+- Appare un timer di 30 secondi
+    - setInterval
+- Spariscono i 5 numeri
+    - innerHTML = '';
+- Appare un alert 5 volte per chiedere i numeri all'utente
+    - num = prompt('Inserisci numero');
+- Risultato: quali e quanti numeri l'utente ha indovinato
+    - arrayNumeriCasuali.includes(num)
 */
 
-/*
+//FUNZIONI
+function getRandomNum(min, max) {
+    return (Math.floor(Math.random() * max) + min);
+}
 
-*/
+function clearDisplay(divContainerEl) {
+    divContainerEl.innerHTML = '';
+}
 
-//Genero un numero casuale da 1 a 100
-function getRandomNum() {
-    return Math.floor((Math.random() * 100) + 1);
-};
-
-//Pulisco l'elemento nella DOM
-function clearDisplay() {
-    const divEl = document.querySelector('.num_generator');
-    divEl.innerHTML = '';
-};
-
-//Acquisisco un numero dall'user
 function getNumUser() {
-    console.log('Sono qui');
-    const num = prompt('Digita numero');
-    return num;
+    return prompt('Digita numero');
 }
 
-//Dichiarazione variabili
-const generatorEl = document.querySelector('.num_generator');
+function pushNumArray(size) {
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+        //console.log('Random num: ', num);
+        arr.push(getRandomNum(1, 100));
+    }
+    return arr;
+}
+
+function getCountdownNum(divContainerEl, divTimerEl, divNumbersEl, array, size) {
+    for (let i = 0; i < size; i++) {
+        divNumbersEl.insertAdjacentHTML('afterbegin', `<div class="slot">${array[i]}</div>`);
+    }
+    let seconds = 4;
+    const timer = setInterval(function () {
+        divTimerEl.innerHTML = seconds;
+        if (seconds === -1) {
+            divTimerEl.innerHTML = '';
+            clearInterval(timer);
+            clearDisplay(divContainerEl);
+        }
+        seconds--;
+    }, 1000);
+    return 0;
+}
+
+//VARIABILI
+const containerEl = document.querySelector('.my_container');
+const numbersEl = document.querySelector('.numbers');
+const timerEl = document.querySelector('.timer');
 const sizeArr = 5;
-let numRandom = [];
-let userNum = [];
-let slot;
+const numArr = pushNumArray(sizeArr);
+let num;
+//passa sizeArr e inserisci ciclo for per inserirli a mano a mano
+//numbersEl.insertAdjacentHTML('afterbegin', `Numeri casuali: ${numArr}`);
+const result = getCountdownNum(containerEl, timerEl, numbersEl, numArr, sizeArr);
 
-//Scorro l'elemento nella DOM per inserire i numeri random
-for (let i = 0; i < sizeArr; i++) {
-    slot = getRandomNum();
-    numRandom.push(slot);
-    generatorEl.insertAdjacentHTML('beforeend', `<div class="slot slot_${i}">${slot}</div>`);
+if (result === 0) {
+    num = getNumUser();
 }
 
-console.log('Array: ', numRandom);
+console.log('Num ', num);
+//const result = getNumUser();
+//clearDisplay(containerEl);
+//console.log('Array: ', numArr);
 
-//Richiamo la funzione per pulire l'elemento della DOM
-setTimeout(clearDisplay, 3000);
 
-//
-for (let i = 0; i < sizeArr; i++) {
-    userNum.push(getNumUser());
-}
-
-console.log('UsernNum: ', userNum);
